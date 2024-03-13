9b29b7918cca78a65c0125e346d92ca9bbaa27b7
Sina Karimi <sinakarimi550@gmail.com>
2024-03-13 18:36:53 -0400

    env setup with Uduino

--- VRGAME/Assets/Scenes/Sina/buttonLED.cs
+++ VRGAME/Assets/Scenes/Sina/buttonLED.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Uduino;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ButtonLED : MonoBehaviour
 {
     public int ledPin = 8; // Pin number of the LED
     public int buttonPin = 7; // Pin number of the button
