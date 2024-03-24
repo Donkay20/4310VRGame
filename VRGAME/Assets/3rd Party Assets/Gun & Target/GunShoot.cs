4e011201eec75e2d6410fa44100fcb05fe361da7
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2024-03-24 15:45:33 -0400

    update enemyhitbox and test UI placeholder

--- VRGAME/Assets/3rd Party Assets/Gun & Target/GunShoot.cs
+++ VRGAME/Assets/3rd Party Assets/Gun & Target/GunShoot.cs
@@ -25,20 +25,15 @@ public class GunShoot : MonoBehaviour
 
     public void Shoot()
     {
-        // 检查是否仍处于冷却时间
         if (lastShot > Time.time)
         {
-            // 尝试射击时，如果处于冷却期，则更新UI显示剩余时间
             UpdateCooldownUI(lastShot - Time.time);
             return;
         }
 
-        // 设置下一次射击的最早时间
         lastShot = Time.time + shootDelay;
-        // 初始化UI显示冷却时间
         UpdateCooldownUI(shootDelay);
         GunShotAudio();
-        // 调整子弹的初始位置，使其在枪口前方10个单位
         Vector3 spawnPosition = bulletPosition.position + bulletPosition.forward * 10;
         var bulletPrefab = Instantiate(bullet, spawnPosition, bulletPosition.rotation);
         var bulletRb = bulletPrefab.GetComponent<Rigidbody>();
@@ -49,23 +44,19 @@ public class GunShoot : MonoBehaviour
 
     private void UpdateCooldownUI(float timeRemaining)
     {
-        // 将显示逻辑封装为一个内部方法，方便复用
         void UpdateText(Text textComponent)
         {
-            if (textComponent != null) // 确保Text组件不为null
+            if (textComponent != null)
             {
-                // 根据剩余时间更新Text组件的文本
                 textComponent.text = timeRemaining <= 0 ? "Ready!" : $"Cooldown: {Mathf.Max(0, timeRemaining).ToString("F2")}s";
             }
         }
 
-        // 更新所有三个Text组件
         UpdateText(cooldownText);
         UpdateText(cooldownText2);
         UpdateText(cooldownText3);
         float quarterCooldown = shootDelay / 4f;
 
-        // 电池按照剩余时间点亮
         bool battery4Active = timeRemaining < quarterCooldown * 4;
         bool battery3Active = timeRemaining < quarterCooldown * 3;
         bool battery2Active = timeRemaining < quarterCooldown * 2;
@@ -74,7 +65,6 @@ public class GunShoot : MonoBehaviour
         SetBatteryActive(battery4Active, battery3Active, battery2Active, battery1Active);
     }
 
-    // 辅助方法来控制电池图标的激活状态
     private void SetBatteryActive(bool b4, bool b3, bool b2, bool b1)
     {
         if (battery1 != null) battery1.enabled = b1;
@@ -86,14 +76,12 @@ public class GunShoot : MonoBehaviour
 
     private void Update()
     {
-        // 如果处于冷却期，更新冷却时间UI
         if (lastShot > Time.time)
         {
             UpdateCooldownUI(lastShot - Time.time);
         }
-        else // 如果不处于冷却期
+        else
         {
-            // 确保所有冷却文本显示"Ready!"
             if (cooldownText != null && cooldownText.text != "Ready!")
             {
                 cooldownText.text = "Ready!";
@@ -112,8 +100,6 @@ public class GunShoot : MonoBehaviour
             SetBatteryActive(true, true, true, true);
         }
     }
-
-
 
 
     private void GunShotAudio ()
