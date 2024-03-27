364e8e82bb0d20e86919041298602d889e9d4448
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2024-03-27 13:11:32 -0400

    add buttonLED functionality with playerinput

--- VRGAME/Assets/Scenes/Sina/buttonLED.cs
+++ VRGAME/Assets/Scenes/Sina/buttonLED.cs
@@ -9,9 +9,12 @@ public class ButtonLEDController : MonoBehaviour
     public int[] ledPins = {3, 5, 7, 9, 11}; // Pin numbers for the LEDs
     bool[] buttonStates = new bool[5]; // Array to track the state of each button
     bool[] ledStates = new bool[5]; // Array to track the state of each LED
+    bool[] isPressed = {false, false, false, false, false}; // Array to track if the LED is pressed
 
     float[] lastButtonPressTimes = new float[5]; // Array to track the time of the last button press for each button
-    float debounceDelay = 0.5f; // Minimum time between button presses to avoid debounce
+    float debounceDelay = 1f; // Minimum time between button presses to avoid debounce
+
+    public PlayerInputPattern inputPattern;
 
     void Start()
     {
@@ -21,6 +24,7 @@ public class ButtonLEDController : MonoBehaviour
             UduinoManager.Instance.pinMode(buttonPins[i], PinMode.Input_pullup);
             UduinoManager.Instance.pinMode(ledPins[i], PinMode.Output);
         }
+
     }
 
     void Update()
@@ -31,14 +35,33 @@ public class ButtonLEDController : MonoBehaviour
             int buttonState = UduinoManager.Instance.digitalRead(buttonPins[i]);
 
             // Check if the button is pressed (assuming LOW means pressed)
-            if (buttonState == 1 && Time.time - lastButtonPressTimes[i] > debounceDelay)
+            if (buttonState == 1 && Time.time - lastButtonPressTimes[i] > debounceDelay && !isPressed[i])
             {
+                isPressed[i] = true;
+
                 // Toggle the LED state
                 ledStates[i] = !ledStates[i];
+
                 UduinoManager.Instance.digitalWrite(ledPins[i], ledStates[i] ? 255 : 0);
 
                 // Update the last button press time
                 lastButtonPressTimes[i] = Time.time;
+
+                inputPattern.UpdatePatternElement(0, 'x', 1);
+            }
+            if (buttonState == 1 && Time.time - lastButtonPressTimes[i] > debounceDelay && isPressed[i])
+            {
+                isPressed[i] = false;
+
+                // Toggle the LED state
+                ledStates[i] = !ledStates[i];
+
+                UduinoManager.Instance.digitalWrite(ledPins[i], ledStates[i] ? 255 : 0);
+
+                // Update the last button press time
+                lastButtonPressTimes[i] = Time.time;
+
+                inputPattern.UpdatePatternElement(0, 'x', 0);
             }
         }
     }
